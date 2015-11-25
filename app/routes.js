228af96b6129d2442c79501d04e227d28d1d9850
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2015-11-25 12:57:17 -0600

    Styles in react

--- app/routes.js
+++ app/routes.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {Route} from 'react-router';
 
 import App from './components/App';
-import Home from './components/home/Home';
+import Main  from './components/home/Main';
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
 
 export default (
     <Route path="/" component={App}>
-        <Route path="/home" component={Home}/>
+        <Route path="/home" component={Main}/>
         <Route path="/about" component={About}/>
         <Route path="/contact" component={Contact}/>
     </Route>
