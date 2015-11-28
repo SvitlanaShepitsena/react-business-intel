a4db07244cbfff97e32e6ff66a8aea15240172b9
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2015-11-28 14:18:30 -0600

    add files

--- app/server.js
+++ app/server.js
@@ -8,20 +8,20 @@ import {RoutingContext, match} from 'react-router';
 import routes from './routes';
 
 const env = process.env;
+const assetsPath = `${env.npm_package_config_appWebpackBaseUrl}/${env.npm_package_version}`;
 const publicPath = path.resolve('../public');
 
 let app = express();
 app.set('trust proxy', 'loopback');
 app.set('x-powered-by', false);
 app.use(express.static(publicPath));
-console.log(publicPath);
 
 global.navigator = {userAgent: 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2454.85 Safari/537.36'};
 app.use((req,res,next)=>{
     var url = req.originalUrl;
     var lastPart = url.split('/').pop();
     if (lastPart.indexOf('.')>-1) {
-        res.sendFile(lastPart, { root: path.join(__dirname, '../public') });
+        res.sendFile(lastPart, { root: path.join(__dirname, `../public/assets/${env.npm_package_version}/`) });
     } else{
         next();
     }
@@ -31,7 +31,6 @@ app.get('/',(req, res, next) => {
 
 
     let location = createLocation(req.originalUrl);
-    var assetsPath = `${req.protocol}://${req.get('host')}/${env.npm_package_version}`;
 
     match({routes, location}, (error, redirectLocation, renderProps) => {
         if (redirectLocation) return res.redirect(redirectLocation.pathname);
@@ -46,16 +45,24 @@ app.get('/',(req, res, next) => {
             `<head>`,
             `<title>${helmet.title}</title>`,
             helmet.meta,
+            helmet.link,
             `<meta charset="utf-8"/>`,
             `<meta property="fb:app_id" content="662941980514705"/>`,
             `<link rel="icon" media="all" type="image/x-icon" href="/favicon.ico"/>`,
             `</head>`,
             `<body>`,
+            `<div id="fb-root"></div>`,
+            `<script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=662941980514705";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>`,
+
             /* server side element*/
             `<div id="app">${markup}</div>`,
             `</body>`,
             /* client side element*/
-            `<script type="text/javascript" src="/app.js"></script>`,
+            `<script type="text/javascript" src="${assetsPath}/app.js"></script>`,
             `</html>`
         ].join('\n');
         res.setHeader('Content-Type', 'text/html');
