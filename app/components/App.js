3c65f2cdac31d0ee32910fb1e5d1f7526d64203b
SvitlanaShepitsena <chicagobusinessintelligence1@gmail.com>
2015-11-27 19:51:31 -0600

    add files

--- app/components/App.js
+++ app/components/App.js
@@ -10,7 +10,6 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state={location : this.props.location};
-        console.log(this.props.location);
     }
 
     render() {
