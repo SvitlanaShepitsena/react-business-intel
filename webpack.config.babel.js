095b49a21df746c5afcb918623ccca76c003ad0d
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2015-11-28 21:12:21 -0600

    Server works with reload

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,34 +1,37 @@
 import path from 'path';
 import webpack from 'webpack';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import OpenBrowserPlugin from 'open-browser-webpack-plugin';
 
 const env = process.env;
 const version = env.npm_package_version;
 const buildPath = env.npm_package_config_appWebpackBuildPath;
 const baseUrl = env.npm_package_config_appWebpackBaseUrl;
 
+
 let config = {
-  entry: {
-    app: path.resolve('app/client.js')
-  },
-  output: {
-    path: path.resolve(`${buildPath}/${version}`),
-    filename: '[name].js',
-    publicPath: `${baseUrl}/${version}/`
-  },
-  module: {
-    loaders: [
-      {test: /\.js(x)?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/},
-      {test: /\.json$/, loaders: ['json']},
-      {test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader')},
-      {test: /\.styl$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!stylus-loader')},
+    entry: {
+        app: path.resolve('app/client.js')
+    },
+    output: {
+        path: path.resolve(`${buildPath}/${version}`),
+        filename: '[name].js',
+        publicPath: `${baseUrl}/${version}/`
+    },
+    module: {
+        loaders: [
+            {test: /\.js(x)?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/},
+            {test: /\.json$/, loaders: ['json']},
+            {test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader')},
+            {test: /\.styl$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!stylus-loader')},
+        ]
+    },
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.EnvironmentPlugin(Object.keys(env)),
+        new OpenBrowserPlugin({url: 'http://localhost:4444'}),
+        new ExtractTextPlugin('[name].css')
     ]
-  },
-  plugins: [
-    new webpack.NoErrorsPlugin(),
-    new webpack.EnvironmentPlugin(Object.keys(env)),
-    new ExtractTextPlugin('[name].css')
-  ]
 };
 
 export default config;
